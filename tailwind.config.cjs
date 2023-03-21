6d0392cda0caab55ae060659b8065a1e70bbdd16
bigboykirusha <105214100+bigboykirusha@users.noreply.github.com>
2023-03-22 00:03:15 +0300

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,22 @@ module.exports = {
   content: ["./src/**/*.{html, htm}"],
   theme: {
     extend: {
-
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'gray': '#808080',
+        'black': 'black',
+      },
+      fontFamily: {
+        'roboto': 'Roboto',
+      },
+      screens: {
+        'tablet': {'max': '991.98px'},
+        // => @media (min-width: 640px) { ... }
+        'mobile': {'max': '767.98px'},
+        // => @media (min-width: 767.98px) { ... }
+      },
     },
   },
   plugins: [],
